513e50b6331bfffb2990fc60a6c435e427c17f8d
Mindaugas Krivickas <mkrivickas@gmail.com>
2024-03-05 22:27:07 +0200

    Number

---
+++ uzd/3-number.js
@@ -0,0 +1,36 @@
+console.clear();
+
+function numberLength(num) {
+  if (typeof num !== "number" || !isFinite(num)) {
+    return "Pateikta netinkamo tipo reikšmė";
+  }
+
+  // Convert number to string to get its length
+  // num = Math.abs(num);
+  // num = num < 0 ? -num : num;
+
+  const digits = "" + num;
+  let count = 0;
+
+  return digits.length;
+}
+
+console.log(numberLength(true)); // Output: Pateikta netinkamo tipo reikšmė
+console.log(numberLength("asd")); // Output: Pateikta netinkamo tipo reikšmė
+console.log(numberLength(NaN)); // Output: Pateikta netinkamo tipo reikšmė
+console.log(numberLength([])); // Output: Pateikta netinkamo tipo reikšmė
+console.log(numberLength({})); // Output: Pateikta netinkamo tipo reikšmė
+console.log(numberLength(undefined)); // Output: Pateikta netinkamo tipo reikšmė
+console.log(numberLength(null)); // Output: Pateikta netinkamo tipo reikšmė
+console.log(numberLength(numberLength)); // Output: Pateikta netinkamo tipo reikšmė
+console.log(numberLength(Infinity)); // Output: Pateikta netinkamo tipo reikšmė
+console.log(numberLength(-Infinity)); // Output: Pateikta netinkamo tipo reikšmė
+
+console.log(numberLength(5)); // Output: 1
+console.log(numberLength(781)); // Output: 3
+console.log(numberLength(37060123456)); // Output: 11
+console.log(numberLength(-5)); // Output: 1
+console.log(numberLength(-781)); // Output: 3
+console.log(numberLength(-37060123456)); // Output: 11
+console.log(numberLength(3.14)); // Output: Pateikta netinkamo tipo reikšmė
+console.log(numberLength(-3.14)); // Output: Pateikta netinkamo tipo reikšmė
