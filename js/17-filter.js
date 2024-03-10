bac02e79f454903bb2a79f05c61965f8be53b211
Mindaugas Krivickas <mkrivickas@gmail.com>
2024-03-10 20:29:08 +0200

    Filter

--- js/17-filter.js
+++ js/17-filter.js
@@ -19,3 +19,38 @@ for (const mark of marks) {
 }
 
 console.log(validMarks);
+
+const filteredMarks = marks // 10
+  .filter((m) => m >= 1) // 7
+  .filter((m) => m <= 10) // 6
+  .filter((m) => m % 1 === 0); // 5
+//   .filter((m) => Number.isInteger(m));
+console.log(filteredMarks);
+
+const filteredMarks2 = marks.filter((m) => m >= 1 && m <= 10 && m % 1 === 0);
+console.log(filteredMarks2);
+
+console.log("------------------");
+const names = [
+  "Petras",
+  "donatas",
+  "sANDRA",
+  "lAuRyNaS",
+  "SiMoNa",
+  "Maryte",
+  "Jonas",
+  "Ona",
+];
+const longNames = names
+  .filter((n) => n.length > 5)
+  .filter((n) => n[0] === n[0].toUpperCase())
+  .filter((n) => n.slice(1) === n.slice(1).toLowerCase());
+
+//   d === d.toUpperCase()
+//   d === D
+
+//   D === D.toUpperCase()
+//   D === D
+
+console.log(names);
+console.log(longNames);
